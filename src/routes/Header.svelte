56c9d77e1cd29516930958a4d035b723d82b3ada
Timothy Mwirabua <techytimo@gmail.com>
2025-08-30 09:33:37 +0300

    testing nav sub

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -4,62 +4,69 @@
 	import { cn } from '$lib/utils';
 
 	let activeUrl = $state(page.url.pathname);
-	let loggedIn = false;
-	let isAdmin = $derived(loggedIn && true);
+	let loggedIn = true;
+	let isAdmin = $derived(loggedIn && false);
 </script>
 
 <header class="flex h-[70px] w-full">
 	<NavigationMenu.Root class="flex max-w-none w-full" value={activeUrl}>
-        <NavigationMenu.List class={cn("flex w-screen justify-between", loggedIn ? "justify-between" : "justify-end")}>
-            
-            {#if isAdmin}
-            <div class="flex">
-                <NavigationMenu.Item>
-                    <NavigationMenu.Link>
-                        <a href="/admin/users" class="flex p-3 hover:bg-gray-100">Users</a>
-                    </NavigationMenu.Link>
-                </NavigationMenu.Item>
-                <NavigationMenu.Item>
-                    <NavigationMenu.Link>
-                        <a href="/admin/projects" class="flex p-3 hover:bg-gray-100">Projects</a>
-                    </NavigationMenu.Link>
-                </NavigationMenu.Item>
-                <NavigationMenu.Item>
-                    <NavigationMenu.Link>
-                        <a href="/admin/subscriptions" class="flex p-3 hover:bg-gray-100">Subscriptions</a>
-                    </NavigationMenu.Link>
-                </NavigationMenu.Item>
-                <NavigationMenu.Item>
-                    <NavigationMenu.Link>
-                        <a href="javascript:void(0)" class="flex p-3 hover:bg-gray-100">Plans</a>
-                    </NavigationMenu.Link>
-                </NavigationMenu.Item>
-            </div>
-            {:else if loggedIn}
+        <NavigationMenu.List class="flex w-screen justify-between">
+                        
             <div class="flex justify-self-start">
-                <NavigationMenu.Item>
+                <NavigationMenu.Item class={cn("justify-self-start")}>
                     <NavigationMenu.Link>
-                        <a href="/projects" class={cn("flex p-3 hover:bg-gray-100", activeUrl == '/projects' && "underline")}>Projects</a>
+                        <a href="/" class="flex p-3 hover:bg-gray-100">Logo</a>
                     </NavigationMenu.Link>
                 </NavigationMenu.Item>
-                <NavigationMenu.Item>
-                    <NavigationMenu.Link>
-                        <a href="/subscriptions" class="flex p-3">
-                            Subscription
-                        </a>
-                    </NavigationMenu.Link>
-                </NavigationMenu.Item>
+
+                {#if isAdmin}
+                    <NavigationMenu.Item>
+                        <NavigationMenu.Link>
+                            <a href="/admin/users" class="flex p-3 hover:bg-gray-100">Users</a>
+                        </NavigationMenu.Link>
+                    </NavigationMenu.Item>
+                    <NavigationMenu.Item>
+                        <NavigationMenu.Link>
+                            <a href="/admin/projects" class="flex p-3 hover:bg-gray-100">Projects</a>
+                        </NavigationMenu.Link>
+                    </NavigationMenu.Item>
+                    <NavigationMenu.Item>
+                        <NavigationMenu.Link>
+                            <a href="/admin/subscriptions" class="flex p-3 hover:bg-gray-100">Subscriptions</a>
+                        </NavigationMenu.Link>
+                    </NavigationMenu.Item>
+                    <NavigationMenu.Item>
+                        <NavigationMenu.Link>
+                            <a href="javascript:void(0)" class="flex p-3 hover:bg-gray-100">Plans</a>
+                        </NavigationMenu.Link>
+                    </NavigationMenu.Item>
+                {:else if loggedIn}
+                    <NavigationMenu.Item>
+                        <NavigationMenu.Link>
+                            <a href="/projects" class={cn("flex p-3 hover:bg-gray-100", activeUrl == '/projects' && "underline")}>Projects</a>
+                        </NavigationMenu.Link>
+                    </NavigationMenu.Item>
+                    <NavigationMenu.Item>
+                        <NavigationMenu.Link>
+                            <a href="/subscriptions" class="flex p-3">
+                                Subscription
+                            </a>
+                        </NavigationMenu.Link>
+                    </NavigationMenu.Item>
+                {/if}
             </div>
-            {/if}
 
             <div class="flex justify-self-end relative">
                 <NavigationMenu.Item class={cn("flex relative left-0 hover:bg-accent nav-container")}>
                     <NavigationMenu.Trigger class={cn("h-full bg-transparent")}>Introduction</NavigationMenu.Trigger>
+                    <!-- <span class="bg-blue-300 p-3 relative left-0 top-15 z-10">
+                        testing
+                    </span> -->
                     <NavigationMenu.Content class={cn("!relative bg-red-300")}>
                         <ul class="grid w-[350px] gap-2 p-4 !relative left-5 bg-green-300">
                             <li>
                                 <NavigationMenu.Link>
-                                    <a href="/about" target="_blank" class="block p-2 hover:bg-gray-100">Sub One</a>
+                                    <a href="/about" target="_blank" class="block p-2 hover:bg-gray-100">Sub 1</a>
                                 </NavigationMenu.Link>
                             </li>
                             <li>
@@ -115,6 +122,7 @@
                     </NavigationMenu.Item>
                 {/if}
             </div>
+            
         </NavigationMenu.List>
     </NavigationMenu.Root>
 	
@@ -125,16 +133,8 @@
 </div>
 
 <style>
-	ul.navul > li {
-		background-color: red;
-	}
-	.bg-primary {
-		-webkit-print-color-adjust: exact;
-		color-adjust: exact;
-	}
-    .nav-container > s {
-        opacity: 1 !important;
-        visibility: visible !important;
-        pointer-events: all !important;
-    }
+:global(li.nav-container > span) {
+    position: absolute !important;
+    left: 0 !important;
+}
 </style>
